6292f569f1efcaa66c71e587928664d6053fad2a
khrisla <107222243+khrisla@users.noreply.github.com>
2025-05-19 21:36:59 -0300

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -4,24 +4,23 @@ class Algorithm:
         self.processes = processes
 
     def FIFO(self):
-        processes = sorted(self.processes)
-        values = 0
-        
-        for i in range(len(self.processes)):
-            processes[i].append(values)
-            values += processes[i][1]
-            processes[i].append(values)
-            
-        return processes
-    
-    def SFJ(self):
-        processes = sorted(self.processes, key=lambda x: x[1])
-        values = 0
-        
-        for i in range(len(self.processes)):
-            processes[i].append(values)
-            values += processes[i][1]
-            processes[i].append(values)
-            
+        processes = sorted(self.processes, key=lambda x: x[0])
+        time = 0
+
+        for i in range(len(processes)):
+            processes[i].append(time)
+            time += processes[i][1]
+            processes[i].append(time)
+
         return processes
 
+    def SFJ(self):
+        processes = sorted(self.processes, key=lambda x: x[1])
+        time = 0
+
+        for i in range(len(processes)):
+            processes[i].append(time)
+            time += processes[i][1]
+            processes[i].append(time)
+
+        return processes
